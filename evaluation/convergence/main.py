2edec3fcfd2866ff8ddd8bef25f7a9b1a6632578
akshat0123 <akshat0123@gmail.com>
2018-05-01 12:48:53 -0400

    Included convergence evaluations results

---
+++ evaluation/convergence/main.py
@@ -0,0 +1,40 @@
+from preprocessor import PreProcessor
+from topic_model import TopicModel
+from tqdm import trange
+from filepaths import *
+from tqdm import tqdm
+import numpy as np
+import pickle
+
+
+
+def main():
+
+    preprocessor = PreProcessor(DICT, CORP, DELIM, RAW, TOKEN, min_count = 10, headword_removal_perc = 0.05)
+    dictionary, corpus, books = preprocessor.get_library_info()
+
+    books = [(book, books[book]['book_tokens']) for book in books]
+    titles, books = zip(*books)
+
+
+    for y_axis_count in tqdm([10000, 20000, 30000]):
+
+        for i in trange(10):
+
+            tm = TopicModel(y_axis_count, 25, 1.0)
+            tm.add_documents(books)
+
+            y_axes = tm.gen_y_axes()
+            topic_freqs, book_topics = tm.calc_trigram_topics(books, y_axes)
+
+            sorted_topics = [(topic, topic_freqs[topic]) for topic in topic_freqs]
+            sorted_topics = sorted(sorted_topics, key=lambda x: x[1], reverse=True)
+            top_percent_count = int(len(sorted_topics) * .05)
+            sorted_topics = sorted_topics[top_percent_count:top_percent_count + 2000]
+
+            for topic in sorted_topics:
+                print('%s|%s|%s' % (y_axis_count, i, topic))
+
+
+if __name__ == '__main__':
+    main()
--- pipeline/method/main.py
+++ pipeline/method/main.py
@@ -14,14 +14,14 @@ def main():
     books = [(book, books[book]['book_tokens']) for book in books]
     titles, books = zip(*books)
 
-    tm = TopicModel(10000, 10, 0.5)
+    tm = TopicModel(10000, 10, 1.0)
     tm.add_documents(books)
 
     y_axes = tm.gen_y_axes()
-    topic_freqs, book_topics = tm.calc_topics(books, y_axes)
+    topic_freqs, book_topics = tm.calc_unigram_topics(books, y_axes)
 
-    sorted_topics = [(topic, topic_freqs[topic]) for topic in topic_freqs]
-    sorted_topics = sorted(sorted_topics, key=lambda x: x[1], reverse=True)[:10]
+    # sorted_topics = [(topic, topic_freqs[topic]) for topic in topic_freqs]
+    # sorted_topics = sorted(sorted_topics, key=lambda x: x[1], reverse=True)[:10]
     # sorted_topics = sorted(sorted_topics, key=lambda x: x[1], reverse=True)
     # top_ten_percent_count = int(len(sorted_topics) * .05)
     # sorted_topics = sorted_topics[top_ten_percent_count:top_ten_percent_count + 20]
